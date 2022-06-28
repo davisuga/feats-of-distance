cb3e57086255da5229fa3d8667376cb27d1552b3
Davi Suga <daviciencia1@gmail.com>
2022-06-28 08:54:18 -0300

    reordered model sequence

--- featsOfDistance/lib/dtos.ml
+++ featsOfDistance/lib/dtos.ml
@@ -1,13 +1,26 @@
 include ArtistDiscographyDto
 
-type profile = { name : string } [@@yojson.allow_extra_fields]
+type album_playability = { playable : bool }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and purple_item = { uri : string; profile : profile }
-[@@yojson.allow_extra_fields]
+type duration = { totalMilliseconds : int }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and track_artists = { items : purple_item list } [@@yojson.allow_extra_fields]
+type profile = { name : string }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and track = {
+type label = string [@@deriving yojson, show]
+
+type content_rating = { label : label }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type purple_item = { uri : string; profile : profile }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type track_artists = { items : purple_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type track = {
   saved : bool;
   uri : string;
   name : string;
@@ -19,59 +32,63 @@ and track = {
   playability : album_playability;
   artists : track_artists;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and domain = {
-  queryArtistDiscographyAlbums : query_artist_discography_albums;
-  queryAlbumTracks : query_album_tracks;
-  queryArtistDiscographyOverview : query_artist_discography_overview;
-  searchDesktop : search_desktop;
-}
-[@@yojson.allow_extra_fields] [@@deriving yojson, show]
+type genres_item = { uid : string; track : track }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and album_playability = { playable : bool } [@@yojson.allow_extra_fields]
+type genres_class = { totalCount : int; items : genres_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and query_album_tracks = { data : data_query_album_tracks }
-[@@yojson.allow_extra_fields]
+type album = { playability : album_playability; tracks : genres_class }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and data_query_album_tracks = { album : album } [@@yojson.allow_extra_fields]
+type data_query_album_tracks = { album : album }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and album = { playability : album_playability; tracks : genres_class }
-[@@yojson.allow_extra_fields]
+type query_album_tracks = { data : data_query_album_tracks }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and genres_class = { totalCount : int; items : genres_item list }
-[@@yojson.allow_extra_fields]
+type data_date = { year : int }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and genres_item = { uid : string; track : track } [@@yojson.allow_extra_fields]
-and content_rating = { label : label } [@@yojson.allow_extra_fields]
-and label = string
-and duration = { totalMilliseconds : int } [@@yojson.allow_extra_fields]
+type fluffy_item = { releases : releases }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and query_artist_discography_albums = {
-  data : data_query_artist_discography_albums;
-}
-[@@yojson.allow_extra_fields]
+type releases = { items : releases_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and data_query_artist_discography_albums = {
-  artist : data_query_artist_discography_albums_artist;
-}
-[@@yojson.allow_extra_fields]
+type purple_albums = { totalCount : int; items : fluffy_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and data_query_artist_discography_albums_artist = {
+type purple_discography = { albums : purple_albums }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type data_query_artist_discography_albums_artist = {
   discography : purple_discography;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and purple_discography = { albums : purple_albums }
-[@@yojson.allow_extra_fields]
+type data_query_artist_discography_albums = {
+  artist : data_query_artist_discography_albums_artist;
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and purple_albums = { totalCount : int; items : fluffy_item list }
-[@@yojson.allow_extra_fields]
+type query_artist_discography_albums = {
+  data : data_query_artist_discography_albums;
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and fluffy_item = { releases : releases } [@@yojson.allow_extra_fields]
-and releases = { items : releases_item list } [@@yojson.allow_extra_fields]
+type all_class = { totalCount : int }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and releases_item = {
+type item_playability = { playable : bool; reason : reason }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type item_date = { year : int; isoString : string }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type releases_item = {
   id : string;
   uri : string;
   name : string;
@@ -81,81 +98,57 @@ and releases_item = {
   sharingInfo : sharing_info;
   tracks : all_class;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and cover_art = { sources : source list } [@@yojson.allow_extra_fields]
+type cover_art = { sources : source list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and source = {
+type visuals = {
+  avatarImage : cover_art option; [@key "avatarImage"] [@yojson.default None]
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type source = {
   url : string;
   width : int option; [@yojson.option]
   height : int option; [@yojson.option]
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and item_date = { year : int; isoString : string } [@@yojson.allow_extra_fields]
+type reason = string
 
-and item_playability = { playable : bool; reason : reason }
-[@@yojson.allow_extra_fields]
+type sharing_info = { shareId : string; shareUrl : string }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and reason = string
-
-and sharing_info = { shareId : string; shareUrl : string }
-[@@yojson.allow_extra_fields]
-
-and all_class = { totalCount : int } [@@yojson.allow_extra_fields]
-
-and query_artist_discography_overview = {
-  data : data_query_artist_discography_overview;
-}
-[@@yojson.allow_extra_fields]
-
-and data_query_artist_discography_overview = {
-  artist : data_query_artist_discography_overview_artist;
-}
-[@@yojson.allow_extra_fields]
-
-and data_query_artist_discography_overview_artist = {
-  id : string;
-  uri : string;
-  profile : profile;
-  discography : fluffy_discography;
-}
-[@@yojson.allow_extra_fields]
-
-and all_albums = ArtistDiscographyDto.artist_discography
-
-and fluffy_discography = {
+type fluffy_discography = {
   albums : ArtistDiscographyDto.artist_discography;
   singles : all_class;
       (* compilations : all_class; *)
       (* all : all_class; *)
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and search_desktop = { data : data_search_desktop }
-[@@yojson.allow_extra_fields]
-
-and data_search_desktop = { searchV2 : search_v2 } [@@yojson.allow_extra_fields]
-
-and search_v2 = {
-  (* albums : search_v2_albums; *)
-  artists : search_v2_artists;
-      (* episodes : episodes; *)
-      (* genres : genres_class; *)
-      (* playlists : playlists; *)
-      (* podcasts : podcasts; *)
-      (* topResults : top_results; *)
-      (* tracks : purple_tracks; *)
-      (* users : users; *)
+type data_query_artist_discography_overview_artist = {
+  id : string;
+  uri : string;
+  profile : profile;
+  discography : fluffy_discography;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and search_v2_albums = { totalCount : int; items : tentacled_item list }
-[@@yojson.allow_extra_fields]
+type data_query_artist_discography_overview = {
+  artist : data_query_artist_discography_overview_artist;
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and tentacled_item = { data : purple_data } [@@yojson.allow_extra_fields]
+type query_artist_discography_overview = {
+  data : data_query_artist_discography_overview;
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and purple_data = {
+type all_albums = ArtistDiscographyDto.artist_discography
+
+type purple_data = {
   __typename : string;
   uri : string;
   name : string;
@@ -163,32 +156,41 @@ and purple_data = {
   coverArt : cover_art;
   date : data_date;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and data_date = { year : int } [@@yojson.allow_extra_fields]
+type tentacled_item = { data : purple_data }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and search_v2_artists = { totalCount : int; items : artist_item list }
-[@@yojson.allow_extra_fields]
+type search_v2_albums = { totalCount : int; items : tentacled_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and artist_item = { data : artist_item_data } [@@yojson.allow_extra_fields]
-
-and artist_item_data = {
+type artist_item_data = {
   __typename : string;
   uri : string;
   profile : profile;
   visuals : visuals;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and visuals = { avatarImage : cover_art option [@yojson.option] }
-[@@yojson.allow_extra_fields]
+type artist_item = { data : artist_item_data }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and episodes = { totalCount : int; items : episodes_item list }
-[@@yojson.allow_extra_fields]
+type search_v2_artists = { totalCount : int; items : artist_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and episodes_item = { data : tentacled_data } [@@yojson.allow_extra_fields]
+type release_date = { isoString : string; precision : string }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and tentacled_data = {
+type podcast_class = {
+  uri : string;
+  name : string;
+  coverArt : cover_art;
+  publisher : profile;
+  mediaType : string;
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type tentacled_data = {
   __typename : string;
   uri : string;
   name : string;
@@ -199,26 +201,21 @@ and tentacled_data = {
   description : string;
   contentRating : content_rating;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and podcast_class = {
-  uri : string;
-  name : string;
-  coverArt : cover_art;
-  publisher : profile;
-  mediaType : string;
-}
-[@@yojson.allow_extra_fields]
+type episodes_item = { data : tentacled_data }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and release_date = { isoString : string; precision : string }
-[@@yojson.allow_extra_fields]
+type episodes = { totalCount : int; items : episodes_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and playlists = { totalCount : int; items : featured_element list }
-[@@yojson.allow_extra_fields]
+type images = { items : cover_art list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and featured_element = { data : featured_data } [@@yojson.allow_extra_fields]
+type owner = { name : string; uri : string; username : string }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and featured_data = {
+type featured_data = {
   __typename : string;
   uri : string;
   name : string;
@@ -226,59 +223,60 @@ and featured_data = {
   images : images;
   owner : owner;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and images = { items : cover_art list } [@@yojson.allow_extra_fields]
+type featured_element = { data : featured_data }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and owner = { name : string; uri : string; username : string }
-[@@yojson.allow_extra_fields]
+type playlists = { totalCount : int; items : featured_element list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and podcasts = { totalCount : int; items : podcasts_item list }
-[@@yojson.allow_extra_fields]
+type podcasts_item = { data : podcast_class }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and podcasts_item = { data : podcast_class } [@@yojson.allow_extra_fields]
+type podcasts = { totalCount : int; items : podcasts_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and top_results = { items : indigo_item list; featured : featured_element list }
-[@@yojson.allow_extra_fields]
-
-and indigo_item = { data : sticky_data } [@@yojson.allow_extra_fields]
-
-and sticky_data = {
-  __typename : string;
-  uri : string;
-  profile : profile option; [@yojson.option]
-  visuals : visuals option; [@yojson.option]
-  id : string option; [@yojson.option]
-  name : string option; [@yojson.option]
-  albumOfTrack : album_of_track option; [@yojson.option]
-  artists : track_artists option; [@yojson.option]
-  contentRating : content_rating option; [@yojson.option]
-  duration : duration option; [@yojson.option]
-  playability : album_playability option; [@yojson.option]
-  description : string option; [@yojson.option]
-  images : images option; [@yojson.option]
-  owner : owner option; [@yojson.option]
-  coverArt : cover_art option; [@yojson.option]
-  date : data_date option; [@yojson.option]
-  publisher : profile option; [@yojson.option]
-  mediaType : string option; [@yojson.option]
-}
-[@@yojson.allow_extra_fields]
-
-and album_of_track = {
+type album_of_track = {
   uri : string;
   name : string;
   coverArt : cover_art;
   id : string;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and purple_tracks = { totalCount : int; items : indecent_item list }
-[@@yojson.allow_extra_fields]
+type top_result_item_data = {
+  __typename : string;
+  uri : string;
+  profile : profile option; [@yojson.option]
+  visuals : visuals option; [@yojson.option]
+      (* id : string option; [@yojson.option] *)
+      (* name : string option; [@yojson.option]
+         albumOfTrack : album_of_track option; [@yojson.option]
+         artists : track_artists option; [@yojson.option]
+         contentRating : content_rating option; [@yojson.option]
+         duration : duration option; [@yojson.option]
+         playability : album_playability option; [@yojson.option]
+         description : string option; [@yojson.option]
+         images : images option; [@yojson.option]
+         owner : owner option; [@yojson.option]
+         coverArt : cover_art option; [@yojson.option]
+         date : data_date option; [@yojson.option]
+         mediaType : string option; [@yojson.option]
+         publisher : profile option; [@yojson.option] *)
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and indecent_item = { data : indigo_data } [@@yojson.allow_extra_fields]
+type top_results_item = { data : top_result_item_data }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and indigo_data = {
+type top_results = {
+  items : top_results_item list;
+  featured : featured_element list;
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type indigo_data = {
   __typename : string;
   uri : string;
   id : string;
@@ -289,14 +287,18 @@ and indigo_data = {
   duration : duration;
   playability : album_playability;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and users = { totalCount : int; items : users_item list }
-[@@yojson.allow_extra_fields]
+type indecent_item = { data : indigo_data }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and users_item = { data : indecent_data } [@@yojson.allow_extra_fields]
+type purple_tracks = { totalCount : int; items : indecent_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and indecent_data = {
+type image = { smallImageUrl : string option; largeImageUrl : string option }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type indecent_data = {
   __typename : string;
   uri : string;
   id : string;
@@ -304,7 +306,37 @@ and indecent_data = {
   username : string;
   image : image;
 }
-[@@yojson.allow_extra_fields]
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
 
-and image = { smallImageUrl : string; largeImageUrl : string }
-[@@yojson.allow_extra_fields]
+type users_item = { data : indecent_data }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type users = { totalCount : int; items : users_item list }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type search_v2 = {
+  (* albums : search_v2_albums; *)
+  artists : search_v2_artists;
+      (* episodes : episodes; *)
+      (* genres : genres_class; *)
+      (* playlists : playlists; *)
+      (* podcasts : podcasts; *)
+      (* topResults : top_results; *)
+      (* tracks : purple_tracks; *)
+      (* users : users; *)
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type data_search_desktop = { searchV2 : search_v2 }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type search_desktop = { data : data_search_desktop }
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
+
+type domain = {
+  queryArtistDiscographyAlbums : query_artist_discography_albums;
+  queryAlbumTracks : query_album_tracks;
+  queryArtistDiscographyOverview : query_artist_discography_overview;
+  searchDesktop : search_desktop;
+}
+[@@deriving yojson, show] [@@yojson.allow_extra_fields]
