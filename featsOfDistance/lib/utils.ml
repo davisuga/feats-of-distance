ed5bef878f57fbb14dde080cf9b689acc5166f4d
Davi Suga <daviciencia1@gmail.com>
2022-06-30 17:00:52 -0300

    feat: added yojson combiner util

--- featsOfDistance/lib/utils.ml
+++ featsOfDistance/lib/utils.ml
@@ -1,1 +1,10 @@
 let flip f x y = f y x
+let ( <% ) = ( |> )
+let ( %> ) = Fun.id
+
+let yojson_concat (yo_list : Yojson.Safe.t) (yo_elem : Yojson.Safe.t) =
+  match yo_list with
+  | `List items -> `List (List.cons yo_elem items)
+  | _ -> `List [ yo_list; yo_elem ]
+
+let yojson_fold = List.fold_left yojson_concat (`List [])
