09b0bff7b833ec5b856b07f87776caedb3caa512
Davi Suga <daviciencia1@gmail.com>
2022-06-30 17:00:38 -0300

    made graphql endpoint to search artists and rest to save an artist

--- featsOfDistance/lib/server.ml
+++ featsOfDistance/lib/server.ml
@@ -1,38 +1,40 @@
-type user = { id : int; name : string }
+open Models
+open Utils
 
-let hardcoded_users = [ { id = 1; name = "alice" }; { id = 2; name = "bob" } ]
-
-let user =
+let artist =
   Graphql_lwt.Schema.(
     obj "user" ~fields:(fun _info ->
         [
-          field "id" ~typ:(non_null int)
+          field "uri" ~typ:(non_null string)
             ~args:Arg.[]
             ~resolve:(fun _info user -> user.id);
           field "name" ~typ:(non_null string)
             ~args:Arg.[]
             ~resolve:(fun _info user -> user.name);
+          field "img" ~typ:string
+            ~args:Arg.[]
+            ~resolve:(fun _info user -> user.img);
         ]))
+
+open! Lwt.Infix
 
 let schema =
   Graphql_lwt.Schema.(
     schema
       [
-        field "users"
-          ~typ:(non_null (list (non_null user)))
-          ~args:Arg.[ arg "id" ~typ:int ]
-          ~resolve:(fun _info () id ->
-            match id with
-            | None -> hardcoded_users
-            | Some id' -> (
-                match
-                  List.find_opt (fun { id; _ } -> id = id') hardcoded_users
-                with
-                | None -> []
-                | Some user -> [ user ]));
+        io_field "artists"
+          ~typ:(non_null (list (non_null artist)))
+          ~args:Arg.[ arg "search_term" ~typ:string ]
+          ~resolve:(fun _info () search_term ->
+            match search_term with
+            | Some term ->
+                Http.search_artists term (* >|= Models.artist_of_yojson *)
+                >|= Array.to_list
+                >|= Result.ok
+            | None -> Lwt.return (Result.Error "Empty search term"));
       ])
 
-let default_query = "{\\n  users {\\n    name\\n    id\\n  }\\n}\\n"
+(* let default_query = "{\\n  users {\\n    name\\n    id\\n  }\\n}\\n" *)
 
 let start port =
   Dream.run ~port
@@ -41,5 +43,14 @@ let start port =
   @@ Dream.router
        [
          Dream.any "/graphql" (Dream.graphql Lwt.return schema);
-         Dream.get "/" (Dream.graphiql ~default_query "/graphql");
+         Dream.get "/" (Dream.graphiql "/graphql");
+         Dream.get "/save_artist/:artist_uri" (fun req ->
+             match Some (Dream.param req "artist_uri") with
+             | Some uri ->
+                 Main.persist_all_tracks_from_artist_id uri
+                 |> Main.string_of_reply_reply_list
+                 >|= yojson_fold
+                 >|= Yojson.Safe.to_string
+                 >>= Dream.json
+             | None -> Dream.respond ~status:`Bad_Request "");
        ]
