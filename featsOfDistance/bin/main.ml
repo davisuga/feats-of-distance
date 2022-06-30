1103e8313fe6e7937961f91688247d333363a7a0
Davi Suga <daviciencia1@gmail.com>
2022-06-30 17:01:16 -0300

    starting server on main

--- featsOfDistance/bin/main.ml
+++ featsOfDistance/bin/main.ml
@@ -1,1 +1,12 @@
-let () = FeatsOfDistance.Main.test () |> Lwt_main.run |> ignore
+(* let sas () =
+     List.map
+       (fun promise -> try Some (Lwt_main.run promise) with _ -> None)
+       (FeatsOfDistance.Main.test ())
+
+   let () = sas () |> ignore *)
+(*
+   let save_artist artist_id =
+     FeatsOfDistance.Main.persist_all_tracks_from_artist_id artist_id
+     |> Option.map Lwt_main.run
+     |> ignore *)
+let () = FeatsOfDistance.Server.start 8080
