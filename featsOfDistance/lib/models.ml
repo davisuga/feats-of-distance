4428e097fe0b74261c9c0d00283716c0c47c3bbc
Davi Suga <daviciencia1@gmail.com>
2022-06-30 17:00:16 -0300

    added yojson to models

--- featsOfDistance/lib/models.ml
+++ featsOfDistance/lib/models.ml
@@ -1,5 +1,11 @@
 (* name * uri *)
-type song_author = string * string
+type song_author = string * string [@@deriving yojson]
+
 type song = { name : string; authors : (string * string) list; id : string }
+[@@deriving yojson]
+
 type album = { name : string; id : string; cover_art_url : string; year : int }
+[@@deriving yojson]
+
 type artist = { name : string; id : string; img : string option }
+[@@deriving yojson]
