b10eda4f0260f0fdaa4c9949844485607993a241
Davi Suga <daviciencia1@gmail.com>
2022-07-03 00:26:28 -0300

    feat: refactored queries to handle songs as relations

--- backend/lib/storage.ml
+++ backend/lib/storage.ml
@@ -1,13 +1,21 @@
 open Models
+open Utils
 
 module Queries = struct
   open Printf
 
-  let create_shortest_path id_a id_b =
+  let create_shortest_path_song_nodes id_a id_b =
     sprintf
       {|MATCH (a1:Author {id:'%s'}),
       (a2:Author {id:'%s'})
        RETURN toJSON(nodes(shortestPath((a1)-[:FEATURES_IN|:HAS_FEATURE*1..100]->(a2))))|}
+      id_a id_b
+
+  let create_shortest_path id_a id_b =
+    sprintf
+      {|MATCH (a1:Author {id:'%s'}),
+          (a2:Author {id:'%s'})
+           RETURN shortestPath((a1)-[:FEATS_WITH*1..100]-(a2))|}
       id_a id_b
 
   let create_multiget ids =
@@ -16,11 +24,6 @@ module Queries = struct
 
   let capture_ids_from_path_query =
     Re2.find_all_exn (Re2.create_exn {|\([0-9]+\)|})
-
-  let get_json_response_from_reply (rply : Redis_lwt.Client.reply) =
-    match rply with
-    | `Multibulk [ _; `Multibulk [ `Multibulk [ `Bulk str ] ]; _ ] -> str
-    | _ -> None
 
   let create_album (album : album) =
     sprintf {|CREATE (:Album {name: "%s", id: "%s", img: "%s" year: "%d"})|}
@@ -44,12 +47,41 @@ module Queries = struct
   let map_snd f (a, b) = (a, f b)
   let snd (_, b) = b
 
+  let create_feat_between_artists (song : song) artist_a_uri artist_b_uri =
+    if artist_a_uri = artist_b_uri then ""
+    else
+      sprintf {|MERGE (%s)-[:FEATS_WITH {name:"%s", id:"%s"}]->(%s)|}
+        artist_a_uri (String.escaped song.name) song.id artist_b_uri
+
   let seq_to_list (seq : 'a Seq.t) =
     Seq.fold_left (fun acc seq_elem -> List.append acc [ seq_elem ]) [] seq
 
+  let make_authors_features authors (song : song) =
+    match authors with
+    | hd :: tail ->
+        tail
+        |> List.map (create_feat_between_artists song hd)
+        |> String.concat "\n"
+    | _ -> ""
+
+  let rec make_authors_features_for_all authors (song : song) =
+    match authors with
+    | head :: tail ->
+        make_authors_features (head :: tail) song
+        :: make_authors_features_for_all tail song
+    | _ -> [ "" ]
+
   let create_song (song : song) =
     let song_authors = song.authors |> List.map (map_snd clean_spotify_uri) in
+    let song_authors_ids = song_authors |> List.map snd in
+    sprintf "%s\n%s"
+      (song.authors |> List.map make_author_merging |> String.concat "\n")
+      (song
+      |> make_authors_features_for_all song_authors_ids
+      |> String.concat "\n")
 
+  let create_song_with_song_nodes (song : song) =
+    let song_authors = song.authors |> List.map (map_snd clean_spotify_uri) in
     sprintf
       {|%s
     MERGE (song:Song {name: "%s", id: "%s"})
@@ -69,9 +101,23 @@ end
 module Redis = struct
   open Redis_lwt
 
+  let get_json_response_from_reply (rply : Redis_lwt.Client.reply) =
+    match rply with
+    | `Multibulk [ _; `Multibulk [ `Multibulk [ `Bulk str ] ]; _ ] -> str
+    | _ -> None
+
+  let redis_host =
+    Sys.getenv_opt "REDIS_HOST" |> Option.value ~default:"localhost"
+
+  let redis_port =
+    Sys.getenv_opt "REDIS_PORT"
+    |> Option.map int_of_string_opt
+    |> Option.join
+    |> Option.value ~default:6379
+
   let ( let* ) = Lwt.bind
   let graph_db_name = "featsOfDistance"
-  let conn = Client.connect { host = "localhost"; port = 6379 }
+  let conn = Client.connect { host = redis_host; port = redis_port }
 
   let run args =
     let* conn = conn in
