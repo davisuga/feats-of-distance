62d2109c17d5141d845d33463e91f750974ca527
Davi Suga <daviciencia1@gmail.com>
2022-07-18 19:42:49 -0300

    fixed lib name

--- backend/bin/main.ml
+++ backend/bin/main.ml
@@ -1,3 +1,4 @@
 let () =
-  FeatsOfDistance.Server.start
-    (FeatsOfDistance.Utils.get_env_var "PORT" ~default:"8081" |> int_of_string)
+  FeatsOfDistanceLib.Server.start
+    (FeatsOfDistanceLib.Utils.get_env_var "PORT" ~default:"8081"
+    |> int_of_string)
