13f5a63093bebad71b83e9f02f14ee7c3908166c
Davi Suga <daviciencia1@gmail.com>
2022-07-18 19:41:32 -0300

    added extraction tests

--- backend/test/featsOfDistance.ml
+++ backend/test/featsOfDistance.ml
@@ -1,30 +1,30 @@
 (* Build with `ocamlbuild -pkg alcotest simple.byte` *)
 
 (* A module with functions to test *)
-open FeatsOfDistance.Storage
+open FeatsOfDistanceLib
+open Storage
 
-(* The tests *)
 let test_create_feat_between_artists () =
   Alcotest.(check string)
     "same string"
+    {|MERGE (artista)-[:FEATS_WITH {name:\"A simple song\", uri:'songuri'}]->(artistb)|}
     (Queries.create_feat_between_artists
        {
          name = "A simple song";
          authors = [ ("author a", "authora"); ("author b", "authorb") ];
-         id = "songid";
+         uri = "songuri";
        }
        "artista" "artistb")
-    {|MERGE (artista)-[:FEATS_WITH {name:"A simple song", id:"songid"}]->(artistb)|}
 
 let test_make_authors_features () =
   Alcotest.(check string)
     "same string"
-    {|MERGE (artista)-[:FEATS_WITH {name:"A simple song", id:"songid"}]->(artistb)
-MERGE (artista)-[:FEATS_WITH {name:"A simple song", id:"songid"}]->(artistc)
-MERGE (artista)-[:FEATS_WITH {name:"A simple song", id:"songid"}]->(artistd)
-MERGE (artistb)-[:FEATS_WITH {name:"A simple song", id:"songid"}]->(artistc)
-MERGE (artistb)-[:FEATS_WITH {name:"A simple song", id:"songid"}]->(artistd)
-MERGE (artistc)-[:FEATS_WITH {name:"A simple song", id:"songid"}]->(artistd)
+    {|MERGE (artista)-[:FEATS_WITH {name:\"A simple song\", uri:'songuri'}]->(artistb)
+MERGE (artista)-[:FEATS_WITH {name:\"A simple song\", uri:'songuri'}]->(artistc)
+MERGE (artista)-[:FEATS_WITH {name:\"A simple song\", uri:'songuri'}]->(artistd)
+MERGE (artistb)-[:FEATS_WITH {name:\"A simple song\", uri:'songuri'}]->(artistc)
+MERGE (artistb)-[:FEATS_WITH {name:\"A simple song\", uri:'songuri'}]->(artistd)
+MERGE (artistc)-[:FEATS_WITH {name:\"A simple song\", uri:'songuri'}]->(artistd)
 
 |}
     (Queries.make_authors_features_for_all
@@ -38,19 +38,44 @@ MERGE (artistc)-[:FEATS_WITH {name:"A simple song", id:"songid"}]->(artistd)
              ("author d", "authord");
              ("author b", "authorb");
            ];
-         id = "songid";
+         uri = "songuri";
        }
     |> String.concat "\n")
+
+open Lwt.Infix
+
+let test_get_artist_discography () =
+  let _ =
+    Http.get_artist_discography_all "spotify:artist:0Efm6O4zn2uSklFoUP7P8x"
+    >|= List.map Openapi.Simplified_album_object.show
+    >|= String.concat "\n"
+    >|= Alcotest.(check string) "same string" "expected"
+    |> Lwt_main.run
+  in
+  ()
+
+let test_query_album_tracks () =
+  Http.get_album_tracks "spotify:album:2ZBRkDrOktMqHA8z6Dhy0K"
+  >|= Dtos.yojson_of_query_album_tracks
+  >|= Yojson.Safe.to_string
+  >|= Alcotest.(check string) "same string" "expected"
+  |> Lwt_main.run
 
 (* Run it *)
 let () =
   let open Alcotest in
-  run "Queries"
+  run "Lib"
     [
-      ( "Author",
+      ( "Author Queries",
         [
           test_case "create_feat_between_artists" `Quick
             test_create_feat_between_artists;
           test_case "make_authors_features" `Quick test_make_authors_features;
+        ] );
+      ( "Artist integration",
+        [
+          test_case "get artist discography" `Slow test_get_artist_discography;
+          test_case "get artist test_query_album_tracks" `Slow
+            test_query_album_tracks;
         ] );
     ]
