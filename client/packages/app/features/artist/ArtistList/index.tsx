a6af37cb0cb48bad02f31ab5490b613088583416
Davi Suga <daviciencia1@gmail.com>
2022-07-20 16:10:19 -0300

    fixed renderItem type

--- client/packages/app/features/artist/ArtistList/index.tsx
+++ client/packages/app/features/artist/ArtistList/index.tsx
@@ -1,5 +1,6 @@
 import { FlatList } from 'dripsy'
 import React from 'react'
+import { ListRenderItem } from 'react-native'
 import { Artist, SearchArtistQuery } from '../../../graphql/client'
 import { ArtistListItem } from '../ArtistListItem'
 type ArtistListProps = {
@@ -8,8 +9,9 @@ type ArtistListProps = {
 }
 
 export const ArtistList = ({ data, onClickItem }: ArtistListProps) => {
-  const renderItem = ({ item }: { item: Artist }) => {
+  const renderItem = ({ item }: Parameters<ListRenderItem<Artist>>[0]) => {
     return <ArtistListItem key={item.uri} data={item} onClick={onClickItem} />
   }
+  //@ts-ignore
   return <FlatList data={data?.artists || []} renderItem={renderItem} />
 }
